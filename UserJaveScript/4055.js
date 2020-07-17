c4b69108fcf36c5a119374d8c3b360004d79f1db
GRAPECITY\Yasuko <example@example.com>
2020-07-17 14:05:50 +0900

    {"Message":"チェックイン","MessageType":2,"Modules":[{"State":1,"Type":1,"Name":"ページ1"},{"State":1,"Type":12,"Name":"70c30736-7f75-4535-8aaa-82f185d976bb"},{"State":3,"Type":2,"Name":"モバイルページ"}],"Version":"v1"}

---
+++ UserJaveScript/4055.js
@@ -0,0 +1,24 @@
+﻿var p = Forguncy.Page;
+p.ready(function () {
+        p.getCell("myCell").bind("mouseenter", function (e) {
+            var textCell = p.getCell("text1");
+            textCell.setValue("マウスがあたってます");
+        }
+       );
+
+        p.getCell("myCell").bind("mouseleave", function (e) {
+            var textCell = p.getCell("text1");
+             textCell.setValue("");
+        }
+        );
+}
+);
+///
+///var p = Forguncy.Page;
+///p.ready(function(){
+///    p.getCell("button1").bind("click",null,function(e){
+///        var textCell = p.getCell("textbox1");
+///        textCell.setValue("test value");
+///    })
+///});
+///
---
+++ UserJaveScript/4103_モバイル.js
@@ -0,0 +1,15 @@
+﻿var p = Forguncy.Page;
+p.ready(function () {
+        p.getCell("momyCell").bind("mouseenter", function (e) {
+            var textCell = p.getCell("motext1");
+            textCell.setValue("マウスがあたってます");
+        }
+       );
+
+        p.getCell("momyCell").bind("mouseleave", function (e) {
+            var textCell = p.getCell("motext1");
+             textCell.setValue("");
+        }
+        );
+}
+);
